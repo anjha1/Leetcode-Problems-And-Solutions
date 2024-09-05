fc4876d913b9db2f1decfce28fc5d72e27e26782
Achhuta Nand Jha <achhutanandj@gmail.com>
2024-09-05 22:21:00 +0530

    Add files via upload
    Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.

--- Leetcode-Solutions/709. To Lower Case/toLlowerCase.java
+++ Leetcode-Solutions/709. To Lower Case/toLlowerCase.java
@@ -1,1 +1,20 @@
 
+class Solution {
+    public String toLowerCase(String s) {
+        char arr[] = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 65 && arr[i] <= 90) {
+                arr[i] = (char) (arr[i] + 32);
+            }
+        }
+        return String.valueOf(arr);
+
+    }
+}
+
+public class toLlowerCase {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        System.out.println(obj.toLowerCase("Anjha"));
+    }
+}
